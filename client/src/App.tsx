2b1c2bd93fbe2342201740ad76407b0941c27a41
Krishna Singha <kumarabhishek000197@gmail.com>
2025-10-23 18:47:02 +0530

    added template

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="bg-gray-300 min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">DAO Treasury Manager!</h1>
+    </div>
+  )
+}
+
+export default App
